6b15aa3159b37999dea9e9710354e3c802674724
shun0918 <s.kitaoka000@gmail.com>
2025-04-20 05:47:05 +0900

    トップレスポンシブ構築

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 window.addEventListener("load", () => {});
+
+document.getElementById('hamburger').addEventListener('click', function () {
+    document.getElementById('nav-menu').classList.toggle('active');
+  });
